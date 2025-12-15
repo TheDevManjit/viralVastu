0fe51b31286dfea2a571e084227dc096a2332ecf
TheDevManjit <manjitkumar1042@gmail.com>
2025-12-15 16:15:37 +0530

    Path fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   build: {
+    outDir: 'dist'
+  },
+  publicDir: 'public',
+  // this ensures redirects file is copied properly
+  assetsInclude: ['_redirects'],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
