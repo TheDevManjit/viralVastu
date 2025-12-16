9d999c9b91b6cc255b1e1f3d23ab25c7eced81f4
TheDevManjit <manjitkumar1042@gmail.com>
2025-12-16 14:17:31 +0530

    try otp fixing

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,27 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-let isConnected = false;
 
 const connectDB = async () => {
-  if (isConnected) {
-    console.log("⚡ Mongo already connected");
-    return;
-  }
 
   try {
-    mongoose.set("strictQuery", true);
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      maxPoolSize: 10,        // 🚀 connection pooling
-      serverSelectionTimeoutMS: 5000,
-    });
-
-    isConnected = true;
-    console.log("✅ MongoDB connected successfully");
+    await mongoose.connect(`${process.env.MONGO_URI}/ecomm`);
+    console.log('mongo db connected successfully')
   } catch (error) {
-    console.error("❌ MongoDB connection failed", error.message);
-    throw error;
+    console.log("Mongo BD connection Failed", error)
   }
-};
+}
 
-export default connectDB;
+export default connectDB
+
+
